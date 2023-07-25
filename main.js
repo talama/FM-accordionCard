e282e5052580a396d766c382998f8c25042094a9
talama <talama@proton.me>
2023-07-25 22:12:34 +0200

    feat: fixed background image and initial faq element initial state

--- main.js
+++ main.js
@@ -2,13 +2,20 @@ window.addEventListener("load", () => {
   const buttons = document.querySelectorAll(
     "button[aria-expanded][aria-controls]"
   );
-  buttons.forEach((btn) => setUp(btn));
+  buttons.forEach((btn) => {
+    if (btn.getAttribute("aria-expanded") === "true") {
+      const content = document.getElementById(
+        btn.getAttribute("aria-controls")
+      );
+      content.style.display = "block";
+    }
+    setUp(btn);
+  });
 });
 
 function setUp(btn) {
   btn.addEventListener("click", () => {
-    const btnId = btn.getAttribute("aria-controls");
-    const content = document.getElementById(btnId);
+    const content = document.getElementById(btn.getAttribute("aria-controls"));
     if (btn.getAttribute("aria-expanded") === "false") {
       content.style.display = "block";
       btn.setAttribute("aria-expanded", "true");
